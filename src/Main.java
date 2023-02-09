802a6d075131465be4853f3ac0f060c7796b4086
orozovgr <orozovgr@mail.uc.edu>
2023-02-09 14:51:53 -0500

    Completed Lab_04_04_EvenOdd

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+      int myNumber = 2;
+      if (myNumber % 2 == 0)
+          System.out.println("number is an even number ");
+      else
+          System.out.println("number is an odd");
+
+    }
+}
